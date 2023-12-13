d48624b1866ed5614bfff3ef079eeaad2b05d4cb
Anshika Bundela <99082647+LAV1E@users.noreply.github.com>
2023-12-13 12:41:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 
+// function minDate(dates) {
+//   var minDt = dates[0],
+//     minDtObj = new Date(dates[0]);
+//   dates.forEach(function (dt, index) {
+//     if (new Date(dt) < minDtObj) {
+//       minDt = dt;
+//       minDtObj = new Date(dt);
+//     }
+//   });
+//   return minDt;
+// }
 function minDate(dates) {
-  var minDt = dates[0],
-    minDtObj = new Date(dates[0]);
-  dates.forEach(function (dt, index) {
-    if (new Date(dt) < minDtObj) {
-      minDt = dt;
-      minDtObj = new Date(dt);
-    }
-  });
-  return minDt;
-}
 
+  var minDt = dates[0];
+
+  var minDtObj = new Date(dates[0]);
+
+ 
+
+  for (var i = 0; i < dates.length; i++) {
+    var currentDate = new Date(dates[i]);
+    if (currentDate < minDtObj) {
+      minDt = dates[i];
+      minDtObj = currentDate;
+    }
+  }
+  return minDt;
+
+}
 
 
 
